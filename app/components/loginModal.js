883f656f21c9a4a3940bbf332c8e24f49e707dc2
Naman-Parlecha <namanparlecha@gmail.com>
2024-04-19 16:17:47 +0530

    login logic

--- app/components/loginModal.js
+++ app/components/loginModal.js
@@ -3,15 +3,30 @@ import classes from "./loginModal.module.css";
 import { IoMdClose } from "react-icons/io";
 import { FaXTwitter } from "react-icons/fa6";
 import { useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+
 export default function LoginModal({ closeModal }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
   function handleCloseLoginModal() {
     closeModal();
   }
-  function handleLogin() {
-    // Handle login logic here
+
+  async function handleLogin() {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      handleCloseLoginModal();
+    } catch (error) {
+      setError(true);
+    }
   }
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
   return (
     <>
       <div className={classes.backdrop}></div>
@@ -26,11 +41,20 @@ export default function LoginModal({ closeModal }) {
         </header>
         <main className={classes.main}>
           <h1 className={classes.title}>Welcome Back</h1>
-          <input placeholder="Email" className={classes.input} name="email" />
+          <input
+            placeholder="Email"
+            className={classes.input}
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
           <input
             placeholder="password"
             className={classes.input}
             name="password"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
           />
           {error && <p className={classes.error}>Invalid User Input</p>}
           <button className={classes.next} onClick={handleLogin}>
