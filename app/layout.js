d3bf26c58235cab2915a46b1d74e3e860394175a
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-04-19 14:26:13 +0530

    sidebar
    Signed-off-by: mohit-nagaraj <mohitnagaraj20@gmail.com>

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./layout.css";
+import Image from "next/image";
+import Link from "next/link";
+import { GoHome } from "react-icons/go";
+import { CgProfile } from "react-icons/cg";
+import { FaSearch } from "react-icons/fa";
+import { FaFeather } from "react-icons/fa";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +18,41 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="sidebar px-8 py-12 h-screen">
+          <div className="flex flex-col gap-8">
+            <div className="logo">
+              <Image src={"/logo.jpg"} width={30} height={30} alt="logo" />
+            </div>
+            <Link href="/home">
+              <div className="logo-item flex gap-2">
+                <GoHome size={20} />
+                <div>Home</div>
+              </div>
+            </Link>
+            <Link href="/explore">
+              <div className="logo-item flex gap-2">
+                <FaSearch size={20} />
+                <div>Explore</div>
+              </div>
+            </Link>
+            <Link href="/profile">
+              <div className="logo-item flex gap-2">
+                <CgProfile size={20} />
+                <div>Profile</div>
+              </div>
+            </Link>
+            <Link href="/post">
+              <div className="button flex logo-item bg-blue-500 gap-2 px-4 py-2 rounded-lg text-center">
+              <FaFeather size={20}/>
+                <div>Post</div>
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        {children}
+      </body>
     </html>
   );
 }
