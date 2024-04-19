764ff1da197958e2709ea442d14dac4d19eeae8e
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-04-19 15:35:50 +0530

    tweet ui fix
    Signed-off-by: mohit-nagaraj <mohitnagaraj20@gmail.com>

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -2,10 +2,11 @@ import { GoDotFill } from "react-icons/go";
 import { FaRegComment, FaRegHeart } from "react-icons/fa";
 import { FiBarChart2 } from "react-icons/fi";
 import { RxLoop } from "react-icons/rx";
+import "./card.css";
 
 const Card = ({ user, tweet }) => {
   return (
-    <div className="bg-gray-800 rounded-lg shadow-md p-4 mt-4 flex gap-4">
+    <div className="bg-gray-900 rounded-lg shadow-md p-4 mt-4 flex gap-4">
       <div>
         <img
           className="h-12 w-12 rounded-full"
@@ -13,7 +14,7 @@ const Card = ({ user, tweet }) => {
           alt="Profile Image"
         />
       </div>
-      <div>
+      <div className="w-full">
         <div className="flex gap-1">
           <p className="font-bold">{user.name}</p>
           <p className="text-gray-500">{"@" + user.username}</p>
@@ -25,20 +26,20 @@ const Card = ({ user, tweet }) => {
           </p>
         </div>
         <p className="text-white">{tweet.content}</p>
-        <div className="flex tweet.info">
+        <div className="flex tweet-info">
           <div className="flex gap-2">
             <FaRegComment size={14}/>
             {tweet.comment??'10'}
           </div>
-          <div className="flex gap-2">
+          <div className="flex gap-2 items-center">
             <RxLoop size={14}/>
             {tweet.repost??'1'}
           </div>
-          <div className="flex gap-2">
+          <div className="flex gap-2 items-center">
             <FaRegHeart size={14}/>
             {tweet.like??'46'}
           </div>
-          <div className="flex gap-2">
+          <div className="flex gap-2 items-center">
             <FiBarChart2 size={14}/>
             {tweet.view??'100'}
           </div>
