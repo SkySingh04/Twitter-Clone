f030d68b2ff36288513e56674b01a74a62fd8db2
Naman-Parlecha <namanparlecha@gmail.com>
2024-04-19 16:15:51 +0530

    login modal

--- app/components/loginModal.js
+++ app/components/loginModal.js
@@ -1,6 +1,43 @@
+"use client";
+import classes from "./loginModal.module.css";
+import { IoMdClose } from "react-icons/io";
+import { FaXTwitter } from "react-icons/fa6";
+import { useState } from "react";
+export default function LoginModal({ closeModal }) {
+  const [error, setError] = useState(false);
 
-export default function loginModal() {
+  function handleCloseLoginModal() {
+    closeModal();
+  }
+  function handleLogin() {
+    // Handle login logic here
+  }
   return (
-    <></>
-  )
+    <>
+      <div className={classes.backdrop}></div>
+      <div className={classes.createCon}>
+        <header className={classes.header}>
+          <button className={classes.close} onClick={handleCloseLoginModal}>
+            <IoMdClose />
+          </button>
+          <div className={classes.logoimg}>
+            <FaXTwitter size="3rem" />
+          </div>
+        </header>
+        <main className={classes.main}>
+          <h1 className={classes.title}>Welcome Back</h1>
+          <input placeholder="Email" className={classes.input} name="email" />
+          <input
+            placeholder="password"
+            className={classes.input}
+            name="password"
+          />
+          {error && <p className={classes.error}>Invalid User Input</p>}
+          <button className={classes.next} onClick={handleLogin}>
+            Login
+          </button>
+        </main>
+      </div>
+    </>
+  );
 }
